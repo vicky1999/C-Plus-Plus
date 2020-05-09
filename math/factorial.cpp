30a809fe79529294ce0fb4b0ddb84037ee0a7f50
Vignesh <ravivignesh2000@gmail.com>
2020-05-09 14:24:29 +0530

    implemented dp with factorials
    the factorial_dp() will return the factorial of the number.  Also, the fact array declared in the global scope will be used to find the factorial of numbers within the number.

--- math/factorial.cpp
+++ math/factorial.cpp
@@ -1,6 +1,16 @@
 // C++ program to find factorial of given number
 #include<iostream>
 
+unsigned int fact[num];
+unsigned int factorial_dp(unsigned int n) {
+    fact[0]=1;
+    fact[1]=1;
+    for(unsigned int i=2;i<=n;i++) {
+        fact[i]=i*fact[i-1];
+    }
+    return fact[n];
+}
+
 // function to find factorial of given number
 unsigned int factorial(unsigned int n) {
     if (n == 0)
@@ -8,10 +18,12 @@ unsigned int factorial(unsigned int n) {
     return n * factorial(n - 1);
 }
 
+
 // Driver code
 int main() {
     int num = 5;
     std::cout << "Factorial of " << num << " is " << factorial(num)
               << std::endl;
+    std::cout << "Factorial of " << num << " is " << factorial_dp(num) << std::endl;
     return 0;
 }
